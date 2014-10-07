a4f4b1c3a76512f17dce4bb36f0bd75f81e7eea8
octoshrimpy <octavo.acapella@gmail.com>
2014-10-06 22:27:06 -0600

    Playing around with enums.
    Figuring out a better system for creating the 21^21 possible combination
    of tools with as less files as possible. May have stumbled into
    something good with storing data in enums.

--- src/tools/items/gadgetOverhaul/RepairStation.java
+++ src/tools/items/gadgetOverhaul/RepairStation.java
@@ -1,9 +1,66 @@
 package tools.items.gadgetOverhaul;
 
+import tools.items.gadgetOverhaul.items.MaterialType;
+
+
 public class RepairStation {
+	
+	MaterialType m;
+	
+	public RepairStation(MaterialType m){ this.m = m; }
+	
+	
 	
 	public void onCraft(){
 		
+		switch(m){
+		case Alumite:
+			MaterialType.Alumite.rod();
+			break;
+		case Iron:
+			MaterialType.Iron.rod();
+			break;
+		case Amethyst:
+			break;
+		case Ardite:
+			break;
+		case BlueSlime:
+			break;
+		case Bone:
+			break;
+		case Bronze:
+			break;
+		case Cactus:
+			break;
+		case Cobalt:
+			break;
+		case Copper:
+			break;
+		case Diamond:
+			break;
+		case Flint:
+			break;
+		case Gold:
+			break;
+		case Manyullyn:
+			break;
+		case Netherrack:
+			break;
+		case Obsidian:
+			break;
+		case Paper:
+			break;
+		case Slime:
+			break;
+		case Steel:
+			break;
+		case Stone:
+			break;
+		case Wood:
+			break;
+		default:
+			break;
+		}
 		
 	}
 	
--- src/tools/items/gadgetOverhaul/items/MaterialType.java
+++ src/tools/items/gadgetOverhaul/items/MaterialType.java
@@ -3,29 +3,41 @@ package tools.items.gadgetOverhaul.items;
 public enum MaterialType {
 
 	//vanilla
-	Iron,
-	Gold,
-	Diamond,
+	Iron 		(4, 5),
+	Gold		(4, 5),
+	Diamond		(4, 5),
 	
 	//metallic non-vanilla
-	Alumite,
-	Ardite,
-	Bronze,
-	Cobalt,
-	Copper,
-	Manyullyn,
-	Steel,
+	Alumite		(4, 5),
+	Ardite		(4, 5),
+	Bronze		(4, 5),
+	Cobalt		(4, 5),
+	Copper		(4, 5),
+	Manyullyn	(4, 5),
+	Steel		(4, 5),
 	
 	//non-metallic
-	Amethyst,
-	BlueSlime,
-	Bone,
-	Cactus,
-	Flint,
-	Netherrack,
-	Obsidian,
-	Paper,
-	Slime,
-	Stone,
-	Wood
+	Amethyst	(4, 5),
+	BlueSlime	(4, 5),
+	Bone		(4, 5),
+	Cactus		(4, 5),
+	Flint		(4, 5),
+	Netherrack	(4, 5),
+	Obsidian	(4, 5),
+	Paper		(4, 5),
+	Slime		(4, 5),
+	Stone		(4, 5),
+	Wood		(4, 5);
+	
+	private final int rod;
+	private final int head;
+	
+	MaterialType(int rod, int head){
+		this.rod = rod;
+		this.head = head;
+	}
+	public int rod(){ return rod; }
+	public int head(){ return head; }
+	
+	
 }
